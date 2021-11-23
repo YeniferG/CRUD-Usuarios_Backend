85e1192d8f9087b72ce1abf2a991b292b21fc77d
Yenifer <guajeyenifer@gmail.com>
2021-11-22 22:40:23 -0500

    Creación del proyecto y su estructura segun video guiado

---
+++ src/main/java/com/sofka/springboot/repositories/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.sofka.springboot.repositories;
+
+import java.util.ArrayList;
+
+import com.sofka.springboot.models.UsuarioModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Long> {
+    public abstract ArrayList<UsuarioModel> findByPrioridad(Integer prioridad);
+}
